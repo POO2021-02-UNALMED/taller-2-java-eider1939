e114acd121c321aebd8c02bf45736e15ee79dd3d
eiderpeña <eapenad@unal.edu.co>
2021-10-19 09:24:00 -0500

    Create Asiento.java

---
+++ src/test/java/test/Asiento.java
@@ -0,0 +1,14 @@
+package test;
+
+public class asiento {
+	String color;
+	int precio;
+	int registro;
+	
+	void cambiarColor(String color) {
+		if (color=="rojo"){
+            
+        }
+	}
+
+}
