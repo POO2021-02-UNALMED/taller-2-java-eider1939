93a0c278886312450f602b8ed5124aa53dffdbb2
eiderpeña <eapenad@unal.edu.co>
2021-10-19 09:37:58 -0500

    Update Motor.java

---
+++ src/test/java/test/Motor.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Motor {
+	int numeroCilindros;
+	String tipo;
+	int registro;
+	
+	void cmabiarRegistro(int registro) {
+		
+	}
+	void asignarTipo(String tipo) {
+		
+	}
+}
